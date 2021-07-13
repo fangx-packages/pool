8bd5a4112b7fd18bfc48feb3b733c4705d36e9c2
nfangxu <nfangxu@gmail.com>
2021-07-13 14:20:25 +0800

    test(limiter): fix test

--- limiter_test.go
+++ limiter_test.go
@@ -1,23 +1,29 @@
 package pool
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
 
 func Test_limiter(t *testing.T) {
+	var wg sync.WaitGroup
+
 	lm := NewLimiter(2)
 
 	fn := func() {
 		defer lm.Release()()
+		defer wg.Done()
 		time.Sleep(time.Second)
 	}
 
 	start := time.Now()
-
 	for i := 0; i < 3; i++ {
+		wg.Add(1)
 		go fn()
 	}
+
+	wg.Wait()
 
 	if time.Since(start) >= 3*time.Second {
 		t.Fail()
